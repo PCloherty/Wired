422e432332933d8109fdd3963db3627e6b623373
Paraic Cloherty <p.cloherty@hotmail.co.uk>
2020-10-09 14:55:40 +0100

    Profile route changes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const auth =require('../../middleware/auth')
 
-//@route Get api/profile
-//@desc test route
-//@access public
-router.get('/', (req, res) => res.send('profile route'));
+const Profile = require('../../models/profile');
+const User = require('../../models/User')
+
+//@route Get api/profile/me
+//@desc get current users profile
+//@access private
+router.get('/me', auth, async ( req, res) => {
+try {
+    const profile = await Profile.findOne({User:req.User.id}).populate(
+        'user',
+        ['name','avatar']
+    )
+    if(!profile) {
+        return res.status(400).json({ msg:'there is no profile for this user'});
+    }
+} catch(err){
+    console.console.error(err.message);
+    res.status(500).send('server Error')
+    }
+}
+)
 
 module.exports=router;
