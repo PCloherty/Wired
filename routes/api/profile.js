9ca02d9575f6f5b73082c4939da4f224729fb1fb
Paraic Cloherty <p.cloherty@hotmail.co.uk>
2020-10-26 14:56:16 +0000

    add gitup route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,7 +4,27 @@ const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
 const Profile = require('../../models/profile');
 const User = require('../../models/User');
-const { Router } = require('express');
+
+//@route GET api/profile/github/:username
+//desc Get user repos from github
+//access public
+router.get('/github/:username', async (req,res) => {
+    try {
+        const uri = encodeURI(
+            `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+        );
+        const headers = {
+            'user-agent' : 'node.js',
+            Authorization: `token ${config.get('githubToken')}`
+        };
+
+        const gitHubResponse = await axios.get(uri, { headers });
+        return res.json(gitHubResponse.data);
+    } catch (err) {
+        console.error(err.message);
+        return res.status(404).json({ msg: 'No Github profile found'})
+    }
+});
 
 //@route Get api/profile/me
 //@desc Get current users profile
@@ -28,8 +48,7 @@ router.get('/me', auth, async (req, res) => {
 //@route Post api/profile
 //@desc Create or update user Profile
 //@access private
-router.post(
-    '/',
+router.post('/',
     [
         //middleware checks
         auth,
