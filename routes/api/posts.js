c356cba9fb0a4158251cc4f806689ef42400334d
Paraic Cloherty <p.cloherty@hotmail.co.uk>
2020-10-28 16:17:36 +0000

    created like and unlike routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -15,7 +15,7 @@ router.post(
     [auth, [check('text', 'Text is required').not().isEmpty()]],
     async (req, res) => {
         const errors = validationResult(req);
-       
+
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
@@ -58,14 +58,14 @@ router.get('/', auth, async (req, res) => {
 router.get('/:id', [auth, checkObjectId('id')], async (req, res) => {
     try {
         const post = await Post.findById(req.params.id);
-        
-        if (!post){
-            return res.status(404).json({msg:'Post not found'});
+
+        if (!post) {
+            return res.status(404).json({ msg: 'Post not found' });
         }
         res.json(post);
     } catch (err) {
         console.error(err.message);
-        
+
         res.status(500).send('Server Error');
     }
 });
@@ -73,27 +73,81 @@ router.get('/:id', [auth, checkObjectId('id')], async (req, res) => {
 //@route DELETE api/posts/:id
 //@desc  delete a post
 //@access Private
-router.delete('/:id',[auth, checkObjectId('id')],  async (req, res) => {
+router.delete('/:id', [auth, checkObjectId('id')], async (req, res) => {
     try {
-      const post = await Post.findById(req.params.id);
-      
-      if (!post) {
-        return res.status(404).json({ msg: 'Post not found' });
-      }
-   
-      // the user deleting the post must be the user who owns the post
-        if(post.user.toString() !== req.user.id) {
-           return res.status(401).json({ msg: 'User not authorized.' });
-        }
-   
-      await post.remove();
-   
-      res.json({ msg: 'Post removed' }); 
-    } catch (err) {
-      console.error(err.message);
-   
-      res.status(500).send('Server Error');
-    }
-  });
+        const post = await Post.findById(req.params.id);
 
+        if (!post) {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+
+        // the user deleting the post must be the user who owns the post
+        if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'User not authorized.' });
+        }
+
+        await post.remove();
+
+        res.json({ msg: 'Post removed' });
+    } catch (err) {
+        console.error(err.message);
+
+        res.status(500).send('Server Error');
+    }
+});
+
+//@route  Put apo/posts/like/:id
+//desc Like a post
+//access private
+router.put('/like/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        //check if the post has already been liked
+        if (
+            post.likes.filter((like) => like.user.toString() === req.user.id)
+                .length > 0
+        ) {
+            return res.status(400).json({ msg: 'Post already liked' });
+        }
+
+        post.likes.unshift({ user: req.user.id });
+
+        await post.save();
+
+        res.json(post.likes);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+//@route  Put apo/posts/unlike/:id
+//desc Unlike a post
+//access private
+router.put('/unlike/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        //check if the post has already been liked
+        if (
+            post.likes.filter((like) => like.user.toString() === req.user.id)
+                .length === 0
+        ) {
+            return res.status(400).json({ msg: 'Post has not yet been liked' });
+        }
+
+        //get remove index
+        const removeIndex = post.likes
+            .map((like) => like.user.toString())
+            .indexOf(req.user.id);
+
+            post.likes.splice(removeIndex,1)
+
+        await post.save();
+
+        res.json(post.likes);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 module.exports = router;
